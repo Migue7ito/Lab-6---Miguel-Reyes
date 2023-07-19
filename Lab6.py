e616cc4019697bf45373abe67139d6397b91eb98
Migue7ito <140018206+Migue7ito@users.noreply.github.com>
2023-07-19 16:37:17 -0400

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,41 @@
+## Lab 6
+
+def encode(string):
+    new = ''
+
+    for num in string:
+
+        if (int(num) + 3) <= 9:  ## checks if current num in encode string when added 3 is less than or equal to 9
+
+            new = new + str((int(num) + 3))
+
+        elif (int(num) + 3) == 12:
+            new = new + str(2)
+        elif (int(num) + 3) == 11:
+            new = new + str(1)
+        elif (int(num) + 3) == 10:
+            new = new + str(0)
+    return new
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n") ## Displays option menu
+
+        option = int(input("Please enter an option: ")) ## prompts user for option input
+
+        if option == 1:
+
+            encode_val = input("Please enter your password to encode: ")  ## prompts user for encode input
+
+            print("Your password has been encoded and stored!\n")
+
+        elif option == 2:
+
+            print(f"The encoded password is {encode(encode_val)}, and the original password is {encode_val}.\n")
+
+        elif option == 3:
+            break
+
